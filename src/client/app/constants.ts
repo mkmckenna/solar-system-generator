331dd5771d7fe26b9c7f7b1f6ebb4d8a06837deb
Martin McKenna <martinmckenna1989@googlemail.com>
2024-04-23 15:20:32 -0400

    Add grid to scene

--- src/client/app/constants.ts
+++ src/client/app/constants.ts
@@ -1,1 +1,2 @@
 export const WIREFRAME = false
+export const GRID = true
--- src/client/app/scene.ts
+++ src/client/app/scene.ts
@@ -1,6 +1,12 @@
 import * as THREE from 'three'
+import { GRID } from './constants'
 
 export const scene = new THREE.Scene()
 
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
+
+if( GRID ) {
+    const gridHelper = new THREE.GridHelper(1000, 10)
+    scene.add(gridHelper) 
+}
