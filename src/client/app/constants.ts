0332af59c97fcc8ee96e9d2fbc6adf104b88d026
Martin McKenna <martinmckenna1989@googlemail.com>
2024-04-24 11:00:38 -0400

    Add toggle to disable skybox

--- src/client/app/constants.ts
+++ src/client/app/constants.ts
@@ -1,11 +1,23 @@
-// DEBUG
+// ---- DEBUG ----
 export const WIREFRAME = false
 export const GRID = true
 
-// PLANET CONSTANTS
-export const MIN_DISTANCE_MULTIPLIER_FROM_STAR = 3 // Measured in star radii
-export const MAX_DISTANCE_MULTIPLIER_FROM_STAR = 20 // Measured in star radii
+// ---- SOLAR SYSTEM CONSTANTS ----
+export const SKYBOX_ENABLED = false
 
-// STAR CONSTANTS
+
+// ---- PLANET CONSTANTS ----
+// Distance from star
+export const MIN_PLANET_COUNT = 1
+export const MAX_PLANET_COUNT = 1
+// Distance from star (star radii multiplier)
+export const MIN_DISTANCE_MULTIPLIER_FROM_STAR = 3
+export const MAX_DISTANCE_MULTIPLIER_FROM_STAR = 20
+// Orbital velocity
+export const MIN_ORBITAL_VELOCITY = 0.01
+export const MAX_ORBITAL_VELOCITY = 0.05
+
+
+// ---- STAR CONSTANTS ----
 export const STAR_RADIUS_MIN = 50
 export const STAR_RADIUS_MAX = 100
--- src/client/app/entities/solar_system.ts
+++ src/client/app/entities/solar_system.ts
@@ -3,25 +3,38 @@ import { Planet } from './planet'
 import { Star } from './star'
 import { Entity } from './entity';
 import { MathUtils } from 'three';
+import { MAX_PLANET_COUNT, MIN_PLANET_COUNT, SKYBOX_ENABLED } from '../constants';
 
 /**
  * Holds all sort of entities that make up a solar system
  */
 export class SolarSystem {
 
+    private entities: Entity[] = []
+
     private skybox = new Skybox()
     private star = new Star()
     private planets: Planet[] = []
 
     constructor() {
-        this.generatePlanets()
+        this.planets = this.generatePlanets()
+
+        if( SKYBOX_ENABLED ) {
+            this.entities.push(this.skybox)
+        }
+        
+        this.entities.push(this.star)
+        this.entities.push(...this.planets)
     }
 
-    private generatePlanets() {
-        const planetCount = MathUtils.randInt(3, 8)
+    private generatePlanets(): Planet[] {
+        const planets: Planet[] = []
+        const planetCount = MathUtils.randInt(MIN_PLANET_COUNT, MAX_PLANET_COUNT)
         for (let i = 0; i < planetCount; i++) {
-            this.planets.push(new Planet( this ))
+            let planet = new Planet(this)
+            planets.push(planet)
         }
+        return planets
     }
 
     public getPlanets(): Planet[] {
@@ -33,6 +46,6 @@ export class SolarSystem {
     }
 
     public getEntities(): Entity[] {
-        return [this.skybox, this.star, ...this.planets]
+        return this.entities
     }
 }
