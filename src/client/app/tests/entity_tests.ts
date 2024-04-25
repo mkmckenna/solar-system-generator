3c7f498b82a8302476afbf897ee920a07bf3e137
Martin McKenna <martinmckenna1989@googlemail.com>
2024-04-24 21:31:34 -0400

    Placeholder test

---
+++ src/client/app/tests/entity_tests.ts
@@ -0,0 +1,7 @@
+// Write a test to check if all solar system entities have a name property set
+// function testEntitiesHaveNameProperty() {
+//     const solarSystem = new SolarSystem();
+//     solarSystem.getEntities().forEach(entity => {
+//         expect(entity.object.name).toBeDefined();
+//     });
+// }
