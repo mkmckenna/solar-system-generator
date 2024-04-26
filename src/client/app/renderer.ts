af2f6204580d62d89d365f9f50eb920eac25c9ac
Martin McKenna <martinmckenna1989@googlemail.com>
2024-04-26 07:31:35 -0400

    New entry point for app

--- src/client/app/renderer.ts
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three'
-
-export const renderer = new THREE.WebGLRenderer();
-
-// Set the size of the renderer to the size of the window
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,62 +1,94 @@
 import * as THREE from 'three'
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-
-import { camera } from './app/camera'
-import { scene } from './app/scene'
-import { renderer } from './app/renderer'
-
-import { SolarSystem, SolarSystemBuilder } from './app/entities/solar_system'
-import { renderSolarSystemInfo } from './app/info'
 import * as cursor from '../client/app/cursor'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GRID } from './app/constants'
+import { SolarSystem, SolarSystemBuilder } from './app/entities/solar_system';
 
 export const modelLoader = new GLTFLoader()
 export const textureLoader = new THREE.TextureLoader()
 
+export class App {
 
-// RENDER LOOP
-const render = function () {
-    requestAnimationFrame( render );
-    renderer.render( scene, camera );
-    solarSystem.entities.forEach( entity => {
-        entity.update()
-    } )
+    public renderer: THREE.WebGLRenderer
+    public scene: THREE.Scene
+    public camera: THREE.PerspectiveCamera
+
+    public solarSystem: SolarSystem
+
+    constructor() {
+        // Scene
+        this.scene = this.createScene()
+        // Renderer
+        this.renderer = this.createRenderer()
+        // Camera
+        this.camera = this.createCamera()
+        // Lighting
+        this.scene.add( this.createLighting() )
+        // Controls
+        new OrbitControls( this.camera, this.renderer.domElement )
+
+        // Build the solar system
+        this.solarSystem = new SolarSystemBuilder().buildRandomSolarSystem()
+
+        this.solarSystem.entities.forEach( entity => {
+            this.scene.add( entity.object );
+        } )
+
+        // Bind the render method to this instance (need to understand this better)
+        this.render = this.render.bind(this)
+        this.render()
+    }
+
+    createLighting(): THREE.Light {
+        const light = new THREE.AmbientLight( 0xffffff, 1 );
+        light.position.set( 0, 1, 0 );
+        return light
+    }
+
+    createScene(): THREE.Scene {
+        const scene = new THREE.Scene()
+
+        // Axis helper
+        const axesHelper = new THREE.AxesHelper(5)
+        scene.add(axesHelper)
+
+        // Grid
+        if( GRID ) {
+            const gridHelper = new THREE.GridHelper(1000, 10)
+            scene.add(gridHelper) 
+        }
+
+        return scene
+    }
+
+    createRenderer(): THREE.WebGLRenderer {
+        const renderer = new THREE.WebGLRenderer()
+        renderer.setSize( window.innerWidth, window.innerHeight )
+        document.body.appendChild( renderer.domElement )
+        return renderer
+    }
+
+    createCamera(): THREE.PerspectiveCamera {
+        const camera = new THREE.PerspectiveCamera( 
+            90, 
+            window.innerWidth / window.innerHeight, 
+            0.1, 
+            5000 )
+        camera.position.set(1000, 1000, 1000)
+        return camera
+    }
+
+    render() {
+        requestAnimationFrame( this.render )
+        this.renderer.render( this.scene, this.camera )
+        // Update entities
+        this.solarSystem.entities.forEach( entity => {
+            entity.update()
+        } )
+    }
 }
 
-// BUILD SOLAR SYSTEM
-const solarSystemBuilder = new SolarSystemBuilder()
-const solarSystem: SolarSystem = solarSystemBuilder.buildRandomSolarSystem()
-solarSystem.entities.forEach( entity => {
-    scene.add( entity.object );
-} )
-
-// LIGHTS
-const light = new THREE.AmbientLight( 0xffffff, 1 );
-light.position.set( 0, 1, 0 );
-scene.add( light );
-
-const controls = new OrbitControls( camera, renderer.domElement );
-render();
+export const app = new App()
 
 cursor.init()
-renderSolarSystemInfo( solarSystem )
-
-
-
-
-// modelLoader.load( 
-//     '/models/weird_planet.gltf', 
-//     function ( gltf ) {
-//         let planet = gltf.scene
-//         planet.position.set( 0, 0, 0 )
-//         planet.scale.set( 1, 1, 1 )
-//         scene.add( planet );
-//     }, 
-//     undefined, 
-//     function ( error ) {
-//         console.log( 'An error happened' )
-//         console.error( error );
-//     } 
-// );
